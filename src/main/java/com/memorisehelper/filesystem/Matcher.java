2448c59b1b8d9c4d3d386415206285e58458f2bf
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-08-09 20:15:02 +0300

    add utils and searchword classes

--- src/main/java/com/memorisehelper/filesystem/Matcher.java
+++
@@ -1,5 +0,0 @@
-package com.memorisehelper.filesystem;
-
-public class Matcher {
-
-}
---
+++ src/main/java/com/memorisehelper/libraries/SearchWord.java
@@ -0,0 +1,47 @@
+package com.memorisehelper.libraries;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class SearchWord {
+
+    private static final String URL_TRANSLATOR = "https://context.reverso.net/перевод/английский-русский/";
+
+    private static void getWord() throws IOException {
+        String url = "https://context.reverso.net/перевод/английский-русский/cat";
+        Document doc = Jsoup.connect(url).get();
+        // Elements elements = doc.select("body");
+        Elements elements = doc
+                .select("body > div[id=wrapper] > section > div[class=left-content] > section[id=top-results]");
+        System.out.println(elements.toString());
+    }
+
+    public List<String> getTranslations(String word) throws IOException {
+        List<String> list = new ArrayList<>();
+        Elements elements = createConnection(word)
+                .select("body > div[id=wrapper] > section > div[class=left-content] > section[id=top-results]" +
+                        " > div[id=translations-content] > a > span[class=display-term]");
+        for (int i = 0; i < 5; i++) {
+            list.add(elements.get(i).text());
+        }
+        return list;
+    }
+
+    private String correctWord(Document document) {
+        Elements elements = document
+                .select("body > div[id=wrapper] > section[id=body-content] > div[class=left-content]" +
+                        " > section[id=top-results] > section[id=search-options] > div[class=title-content]");
+        return elements.toString();
+    }
+
+    private Document createConnection(String word) throws IOException {
+        String url = URL_TRANSLATOR + word;
+        return Jsoup.connect(url).get();
+
+    }
+}
